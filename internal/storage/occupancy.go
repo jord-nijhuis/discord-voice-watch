de3943ebe21570b1b1a019ecb465e6ffffa16336
Jord Nijhuis <jord@nijhuis.me>
2024-12-08 18:14:26 +0100

    Add a RW lock for the occupancy

--- internal/storage/occupancy.go
+++ internal/storage/occupancy.go
@@ -1,13 +1,17 @@
 package storage
 
+import "sync"
+
 var (
 	// channelOccupancy is a map of guild IDs to the number of users in voice channels
 	channelOccupancy = make(map[string]int)
+	lock             = sync.RWMutex{}
 )
 
 func GetOccupancy(guildID string) int {
-
+	lock.RLock()
 	val, ok := channelOccupancy[guildID]
+	lock.RUnlock()
 
 	if ok {
 		return val
@@ -18,10 +22,15 @@ func GetOccupancy(guildID string) int {
 
 func IncrementOccupancy(guildID string) {
 
+	lock.Lock()
 	channelOccupancy[guildID]++
+	lock.Unlock()
 }
 
 func DecrementOccupancy(guildID string) {
 	// Decrement but not below 0
+	lock.Lock()
 	channelOccupancy[guildID] = max(channelOccupancy[guildID]-1, 0)
+
+	lock.Unlock()
 }
